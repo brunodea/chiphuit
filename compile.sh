aedd011021929cb0ff954937a7906e8fc7877147
Bruno Romero de Azevedo <brunordea@gmail.com>
2016-07-17 15:20:47 -0300

    Add support for Debugging in the compile script.

--- compile.sh
+++ compile.sh
@@ -1,2 +1,12 @@
-cmake -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -H. -Bbuild
+
+case $1 in
+    "-d") # debug mode
+        cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -H. -Bbuild
+    ;;
+    *)
+        cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -H. -Bbuild
+    ;;
+esac
+
 cmake --build build -- -j3
+
